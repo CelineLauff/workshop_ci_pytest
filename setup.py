b6cb21e95ba6fbf27d226a01bf63696a336010c1
CelineLauff <107989143+CelineLauff@users.noreply.github.com>
2022-06-22 10:56:38 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+  name="my_package",
+  version="0.0.1",
+  author="Julian Karl Bauer",
+  author_email="...",
+  description="Addition",
+  packages=["my_package"],
+  install_requires=[],
+  ...
+)
