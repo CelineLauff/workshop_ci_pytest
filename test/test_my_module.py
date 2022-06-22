c8749e513f83dffea28b01d2a2e1e41c00edc614
CelineLauff <107989143+CelineLauff@users.noreply.github.com>
2022-06-22 11:29:03 +0200

    Update test_my_module.py

--- test/test_my_module.py
+++ test/test_my_module.py
@@ -1,4 +1,11 @@
 from my_package.my_module import add
 
+import numpy as np
+
 def test_add():
   assert add(3,4) == 7
+  
+def show_printing():
+  matrix=np.random.rand(3,3)
+  print(matrix)
+  assert False
